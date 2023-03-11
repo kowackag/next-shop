1cfdb61822213298b086dcc1e952c6af69c6a607
mkowack <kowackag@gmail.com>
2023-03-11 20:17:37 +0100

    Add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <p className="text-center font-bold border-8">Hejka</p>
+      <Component {...pageProps} />;
+    </>
+  );
 }
+
+
---
+++ src/pages/users.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>!!!!!!!!!!!!!!!!!!!User!!!!</h2>
+      <p>Mam na imię Gosia</p>
+    </div>
+  );
+};
+
+export default AboutPage;
