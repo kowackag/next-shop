4b5389f2cd8839dc78bc2129604d4b16863ffe0c
mkowack <kowackag@gmail.com>
2023-03-17 23:24:00 +0100

    Change NavLinks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,53 +4,32 @@ import { useRouter } from "next/router";
 
 export const Header = () => {
   const router = useRouter();
+
+  const links = [
+    { title: "Home", href: "/" },
+    { title: "Women", href: "/women" },
+    { title: "Men", href: "/men" },
+    { title: "Inspiration", href: "/inspiration" },
+    { title: "Shops", href: "/shops" },
+  ];
+
   return (
     <header className="px-4 py-2 bg-slate-50 text-2xl text-zinc-800 flex justify-between items-center shadow-md">
       <div>
         <p>Shops</p>
       </div>
       <nav className="w-1/2">
-        <ul className="px-4 py-2 bg-slate-50 text-xl text-zinc-500 uppercase flex justify-between">
-          <li className=" hover:text-zinc-800 ">
-            <Link
-              href="/"
-              className={router.pathname === "/" ? "selected" : ""}
-            >
-              Home
-            </Link>
-          </li>
-          <li className=" hover:text-zinc-800 ">
-            <Link
-              href="/women"
-              className={router.pathname === "/women" ? "selected" : ""}
-            >
-              Women
-            </Link>
-          </li>
-          <li className=" hover:text-zinc-800 ">
-            <Link
-              href="/men"
-              className={router.pathname === "/men" ? "selected" : ""}
-            >
-              Men
-            </Link>
-          </li>
-          <li className=" hover:text-zinc-800 ">
-            <Link
-              href="/inspiration"
-              className={router.pathname === "/inspiration" ? "selected" : ""}
-            >
-              Inspiration
-            </Link>
-          </li>
-          <li className=" hover:text-zinc-800 ">
-            <Link
-              href="/about"
-              className={router.pathname === "/about" ? "selected" : ""}
-            >
-              About
-            </Link>
-          </li>
+        <ul className="px-4 py-2 bg-slate-50 text-xl text-zinc-400 uppercase flex justify-between">
+          {links.map(({ href, title }) => (
+            <li className=" hover:text-zinc-800 " key={href}>
+              <Link
+                href={href}
+                className={router.pathname === href ? "text-zinc-800" : ""}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <div>
