a936a28423fac63b3f2f008a290a7e108f2d3b78
mkowack <kowackag@gmail.com>
2023-03-28 21:00:32 +0200

    Isrmemory cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // Defaults to 50MB
+    isrMemoryCacheSize: 0,
+  },
   trailingSlash: true,
   images: {
     domains: [
