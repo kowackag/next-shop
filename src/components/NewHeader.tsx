257b16fb8b1fe80e81588b8487a2adb39cd88b99
mkowack <kowackag@gmail.com>
2023-05-05 19:37:27 +0200

    products.tsx - graphql

--- src/components/NewHeader.tsx
+++ src/components/NewHeader.tsx
@@ -15,8 +15,8 @@ export const NewHeader = () => {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 stroke-width="2"
                 d="M4 6h16M4 12h16M4 18h16"
               />
@@ -76,8 +76,8 @@ export const NewHeader = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       stroke-width="2"
                       d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                     />
@@ -100,8 +100,8 @@ export const NewHeader = () => {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       stroke-width="2"
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     />
@@ -124,8 +124,8 @@ export const NewHeader = () => {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       stroke-width="2"
                       d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                     />
---
+++ src/pages/products.tsx
@@ -0,0 +1,78 @@
+import { InferGetStaticPropsType, GetStaticPropsContext } from "next";
+import React from "react";
+import { Pagination } from "src/components/Pagination/Pagination";
+import { Product } from "src/components/Product";
+import { apolloClient } from "src/graphql/apolloClient";
+import { gql } from "@apollo/client";
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  images: {
+    url: string;
+  }[];
+}
+
+interface GetProductListResponse {
+  products: Product[];
+}
+
+const Products = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  if (!data) {
+    return (
+      <div className="flex-grow px-8 py-8 text-2xl text-zinc-700">
+        <p>Loading....</p>
+      </div>
+    );
+  }
+  return (
+    <div className="px-8 py-8 mx-2 flex">
+      <div className="hidden lg:block w-1/4 text-4xl">Filters</div>
+      <div className="grow">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center grow">
+          {data && data.products.map((item) => (
+            <li key={item.id} className="flex">
+              <Product
+                id={item.id}
+                title={item.name}
+                price={item.price}
+                newPrice={item.price}
+                image={[{ src: item.images[0].url, alt: item.name }]}
+                // colors={item.colors || ["red"]}
+                // sizes={item.sizes || ["s"]}
+              />
+            </li>
+          ))}
+        </ul>
+        <Pagination path="men" length={10} />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
+
+export const getStaticProps = async () => {
+  const { data } = await apolloClient.query<GetProductListResponse>({
+    query: gql`
+      query getProduts {
+        products {
+          id
+          name
+          price
+          images {
+            url
+            width
+            height
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      data,
+    },
+  };
+};
